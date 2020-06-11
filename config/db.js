25857d3e222e2bf52b0b86f811c34246ee30ab78
tichnas <sanchit.arora.2002@gmail.com>
2020-06-11 21:50:54 +0530

    Basic DB & Server setup

---
+++ config/db.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const db = process.env.mongoURI;
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+
+        console.log('MongoDB Connected...');
+    } catch (error) {
+        console.error(error.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
