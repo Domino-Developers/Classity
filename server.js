108ad161f323622f4075517e05ca88374df51512
gsc2001 <gurkirat.singh@students.iiit.ac.in>
2020-06-13 01:03:58 +0530

    [Add] Add course route

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.json());
 // Connecting routers
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/course', require('./routes/api/course'));
 
 const PORT = process.env.PORT || 5000;
 
