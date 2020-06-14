2b54a221a02ac492b184ec4e00b5c6531bfa0e95
tichnas <sanchit.arora.2002@gmail.com>
2020-06-15 01:10:36 +0530

    doubts / dump-resource Schema Fixes #39

---
+++ models/Comment.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const CommentSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    topic: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'topic'
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'user'
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    reply: [
+        {
+            userId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'user'
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ]
+});
+
+module.exports.doubt = mongoose.model('doubt', CommentSchema);
+module.exports.resourceDump = mongoose.model('resourceDump', CommentSchema);
