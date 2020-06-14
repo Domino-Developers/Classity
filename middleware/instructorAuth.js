1db9a37381f46a9894f2420eaafbc6fa66e1c588
tichnas <sanchit.arora.2002@gmail.com>
2020-06-14 14:44:33 +0530

    Add topicId in instructoreAuth

--- middleware/instructorAuth.js
+++ middleware/instructorAuth.js
@@ -1,4 +1,5 @@
 const Course = require('../models/Course');
+const Topic = require('../models/Topic');
 
 /**
  *
@@ -8,21 +9,28 @@ const Course = require('../models/Course');
  * @description middleware to check if the user is instructor of course
  */
 const instructorAuth = async (req, res, next) => {
-    const courseId = req.params.courseId;
+    let { courseId, topicId } = req.params;
 
-    if (!courseId) {
+    if (!courseId && !topicId) {
         return res.status(400).json({
-            msg: 'Course Id not found'
+            msg: 'Course Id or Topic Id not found'
         });
     }
 
     try {
+        if (!courseId) {
+            const topic = await Topic.findById(topicId).select('course');
+            courseId = topic.course;
+        }
+
         const course = await Course.findById(courseId).select('instructor');
+
         if (course.instructor.toString() !== req.user.id) {
             return res
                 .status(401)
                 .json({ msg: 'Not authorized to edit course' });
         }
+
         next();
     } catch (err) {
         return res.status(500).json({ msg: 'Server error' });
