7afa7a2e9dbc57750e7f9e892e4172ed3deade63
tichnas <sanchit.arora.2002@gmail.com>
2020-06-15 01:48:29 +0530

    Add topicId to studentAuth

--- middleware/studentAuth.js
+++ middleware/studentAuth.js
@@ -8,17 +8,25 @@ const Course = require('../models/Course');
  * @description middleware to check if the student is enrolled in course or not
  */
 const studentAuth = async (req, res, next) => {
-    const courseId = req.params.courseId;
+    let { courseId, topicId } = req.params;
 
-    if (!courseId) {
-        return res.status(400).json({ msg: 'Course Id not found' });
+    if (!courseId && !topicId) {
+        return res.status(400).json({
+            msg: 'Course Id or Topic Id not found'
+        });
     }
+
     try {
+        if (!courseId) {
+            const topic = await Topic.findById(topicId).select('course');
+            courseId = topic.course;
+        }
+
         const course = await Course.findById(courseId).select('students');
         if (!course) {
             return res
                 .status(400)
-                .json({ errors: [{ msg: 'course_id not found' }] });
+                .json({ errors: [{ msg: 'Course not found' }] });
         }
         if (
             course.students.filter(id => id.toString() === req.user.id)
@@ -26,14 +34,15 @@ const studentAuth = async (req, res, next) => {
         ) {
             return res.status(401).json({ msg: 'Not authorised' });
         }
+
         next();
     } catch (err) {
         if (err.kind === 'ObjectId') {
             return res
                 .status(400)
-                .json({ errors: [{ msg: 'incorrect course_id' }] });
+                .json({ errors: [{ msg: 'Invalid Course Id' }] });
         }
-        return res.status(500).json({ msg: 'Server error' });
+        return res.status(500).json({ msg: 'Server Error' });
     }
 };
 
