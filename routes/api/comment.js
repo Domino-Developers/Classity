9d0e91ff428e3c24612e684643de187f75b8cf4f
tichnas <sanchit.arora.2002@gmail.com>
2020-06-16 00:51:15 +0530

    Add reply to comment

--- routes/api/comment.js
+++ routes/api/comment.js
@@ -13,6 +13,59 @@ const router = express.Router();
 // ----------------------------------- Routes ------------------------------------------
 
 /**
+ * @route		PUT api/comment/:commentId/like
+ * @description Like a comment
+ * @access		private + classroomOnly
+ */
+
+router.put('/:commentId/like', [auth, classroomAuth], async (req, res) => {
+    try {
+        const comment = await Comment.findById(req.params.commentId);
+
+        if (
+            comment.likes.findIndex(user => String(user) === req.user.id) === -1
+        ) {
+            comment.likes.push(req.user.id);
+
+            await comment.save();
+
+            res.json(comment.likes);
+        } else {
+            res.status(400).json({ msg: 'Comment already liked' });
+        }
+    } catch (err) {
+        res.status(500).json({ msg: 'Server Error' });
+    }
+});
+
+/**
+ * @route		DELETE api/comment/:commentId/like
+ * @description Unlike a comment
+ * @access		private + classroomOnly
+ */
+
+router.delete('/:commentId/like', [auth, classroomAuth], async (req, res) => {
+    try {
+        const comment = await Comment.findById(req.params.commentId);
+        const index = comment.likes.findIndex(
+            user => String(user) === req.user.id
+        );
+
+        if (index !== -1) {
+            comment.likes.splice(index, 1);
+
+            await comment.save();
+
+            res.json(comment.likes);
+        } else {
+            res.status(400).json({ msg: 'Comment not liked' });
+        }
+    } catch (err) {
+        res.status(500).json({ msg: 'Server Error' });
+    }
+});
+
+/**
  * @route		PUT api/comment/:commentId/reply
  * @description Reply to a comment
  * @access		private + classroomOnly
