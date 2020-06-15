6902d83a7b5210028247ddc9638d872c0ce51342
tichnas <sanchit.arora.2002@gmail.com>
2020-06-15 19:07:18 +0530

    temp

--- routes/api/topic.js
+++ routes/api/topic.js
@@ -4,11 +4,12 @@ const { check, validationResult } = require('express-validator');
 // middlewares
 const auth = require('../../middleware/auth');
 const instructorAuth = require('../../middleware/instructorAuth');
+const studentAuth = require('../../middleware/studentAuth');
 
 // Models
-const Course = require('../../models/Course');
 const User = require('../../models/User');
 const Topic = require('../../models/Topic');
+const Comment = require('../../models/Comment');
 
 // Initialize router
 const router = express.Router();
@@ -56,5 +57,51 @@ router.put(
         }
     }
 );
+
+/**
+ * @route		PUT api/topic/:topicId/resourceDump + PUT api/topic/:topicId/doubt
+ * @description Add/Update user's resource/doubt
+ * @access		private + studentOnly
+ */
+
+const addComment = async (type, user, topicId, text) => {
+    const arrObj = await Topic.findById(topicId)
+        .populate({ path: type })
+        .select(type);
+
+    const arr = arrObj[type];
+    const index = arr.findIndex(e => String(e.user) === user);
+
+    if (index === -1) {
+        const comment = new Comment[type]({ user, text });
+        await comment.save();
+        arr.push(comment.id);
+        await Topic.findOneAndUpdate({ _id: topicId }, { [type]: arr });
+    } else {
+        arr[index].text = text;
+        await Comment[type].findOneAndUpdate({ _id: arr[index].id }, { text });
+    }
+
+    return arr;
+};
+
+router.put('/:topicId/doubt', [auth, studentAuth], async (req, res) => {
+    try {
+        const arr = await addComment(
+            'doubts',
+            req.user.id,
+            req.params.topicId,
+            'hello'
+        );
+        console.log(arr);
+
+        res.send(arr);
+    } catch (err) {
+        if (err.message === 'Bad Request') {
+            return res.status(400).json({ msg: err.message });
+        }
+        res.status(500).json({ msg: 'Server Error' });
+    }
+});
 
 module.exports = router;
