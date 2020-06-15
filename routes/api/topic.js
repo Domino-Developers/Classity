c8eec0b3481059bc0cbc0f74a50d603a55d77ba4
gsc2001 <gurkirat.singh@students.iiit.ac.in>
2020-06-16 02:33:43 +0530

    [Add] Add Test route

--- routes/api/topic.js
+++ routes/api/topic.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator');
+const mongoose = require('mongoose');
 
 // middlewares
 const auth = require('../../middleware/auth');
@@ -10,6 +11,7 @@ const studentAuth = require('../../middleware/studentAuth');
 const User = require('../../models/User');
 const Topic = require('../../models/Topic');
 const Comment = require('../../models/Comment');
+const Test = require('../../models/Test');
 
 // Initialize router
 const router = express.Router();
@@ -38,7 +40,7 @@ router.put(
                     kind: resource.kind,
                     text: resource.payload,
                     url: resource.payload,
-                    testId: resource.payload
+                    testId: mongoose.Types.ObjectId(resource.payload)
                 };
             });
 
@@ -50,8 +52,8 @@ router.put(
 
             res.json(topic.coreResources);
         } catch (err) {
-            if (err.message === 'Bad Request') {
-                return res.status(400).json({ msg: err.message });
+            if (err.message === 'Bad Request' || err.kind === 'ObjectId') {
+                return res.status(400).json({ msg: 'Invalid data' });
             }
             res.status(500).json({ msg: 'Server Error' });
         }
@@ -89,6 +91,52 @@ router.put(
             res.json(newTopic[type]);
         } catch (err) {
             console.error(err);
+            res.status(500).json({ msg: 'Server Error' });
+        }
+    }
+);
+/*
+ * @route		POST api/topic/:topicId/test
+ * @description Add a test and get test-id
+ * @access		private + instructorOnly
+ */
+router.post(
+    '/:topicId/test',
+    [
+        auth,
+        instructorAuth,
+        [
+            check('name', "Can't be empty").not().isEmpty(),
+            check('questions', "questions can't be empty").not().isEmpty()
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        try {
+            // Create test object
+            const test = new Test({
+                ...req.body,
+                topic: req.params.topicId
+            });
+
+            // check for errors
+            const ValidationErrors = test.validateSync();
+            if (ValidationErrors) {
+                const error_messages = {},
+                    errors = ValidationErrors.errors;
+                for (let field in errors)
+                    error_messages[field] = errors[field].message;
+                return res.status(400).json({ errors: error_messages });
+            }
+
+            // save the test
+            await test.save();
+            res.json(test);
+        } catch (err) {
+            console.error(err.message);
             res.status(500).json({ msg: 'Server Error' });
         }
     }
