55fbedd02d717655f466fe3c824878efde1a83dc
gsc2001 <gurkirat.singh@students.iiit.ac.in>
2020-06-17 01:13:35 +0530

    [Add] Update Test, Put score routes

--- middleware/instructorAuth.js
+++ middleware/instructorAuth.js
@@ -1,5 +1,6 @@
 const Course = require('../models/Course');
 const Topic = require('../models/Topic');
+const Test = require('../models/Test');
 
 /**
  *
@@ -9,25 +10,37 @@ const Topic = require('../models/Topic');
  * @description middleware to check if the user is instructor of course
  */
 const instructorAuth = async (req, res, next) => {
-    let { courseId, topicId } = req.params;
+    let { courseId, topicId, testId } = req.params;
 
-    if (!courseId && !topicId) {
+    if (!courseId && !topicId && !testId) {
         return res.status(400).json({
-            msg: 'Course Id or Topic Id not found'
+            msg: 'Course Id or Topic Id or Test Id not found'
         });
     }
 
     try {
         if (!courseId) {
+            if (!topicId) {
+                const test = await Test.findById(testId).select('topic');
+                if (!test) {
+                    return res
+                        .status(400)
+                        .json({ errors: [{ msg: 'Invalid Id' }] });
+                }
+                topicId = test.topic;
+            }
             const topic = await Topic.findById(topicId).select('course');
+            if (!topic) {
+                return res
+                    .status(400)
+                    .json({ errors: [{ msg: 'Invalid Id' }] });
+            }
             courseId = topic.course;
         }
 
         const course = await Course.findById(courseId).select('instructor');
         if (!course) {
-            return res
-                .status(400)
-                .json({ errors: [{ msg: 'Course not found' }] });
+            return res.status(400).json({ errors: [{ msg: 'Invalid Id' }] });
         }
         if (course.instructor.toString() !== req.user.id) {
             return res
@@ -38,9 +51,7 @@ const instructorAuth = async (req, res, next) => {
         next();
     } catch (err) {
         if (err.kind === 'ObjectId') {
-            return res
-                .status(400)
-                .json({ errors: [{ msg: 'Invalid Course Id' }] });
+            return res.status(400).json({ errors: [{ msg: 'Invalid Id' }] });
         }
         return res.status(500).json({ msg: 'Server Error' });
     }
