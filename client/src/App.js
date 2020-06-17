be809a16caece70c479b8fe933d2aed810ec04ee
tichnas <sanchit.arora.2002@gmail.com>
2020-06-17 22:34:30 +0530

    React Cleanup

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+function App() {
+    return (
+        <Fragment>
+            <h1>Summer Up</h1>
+        </Fragment>
+    );
+}
+
+export default App;
