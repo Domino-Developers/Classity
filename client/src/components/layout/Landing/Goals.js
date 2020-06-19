2d654df3795d82fb15f60c2d1dcfe44c2eee8385
Sanchit Arora <56799401+tichnas@users.noreply.github.com>
2020-06-20 02:09:11 +0530

    [Add] Homepage Component to show goals (#124)

---
+++ client/src/components/layout/Landing/Goals.js
@@ -0,0 +1,49 @@
+import React, { Fragment } from 'react';
+
+const Goals = () => (
+    <Fragment>
+        <h2>Achieve your goals with Study Tube</h2>
+        <div className='cards'>
+            <div className='card'>
+                <img src={require('./img/skill.png')} alt='Skill icon' />
+                <h3>Learn the latest skills</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                    auctor scelerisque nunc sit amet posuere.
+                </p>
+            </div>
+            <div className='card'>
+                <img src={require('./img/career.png')} alt='Career icon' />
+                <h3>Get ready for a career</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                    auctor scelerisque nunc sit amet posuere.
+                </p>
+            </div>
+            <div className='card'>
+                <img
+                    src={require('./img/certificate.png')}
+                    alt='Certificate icon'
+                />
+                <h3>Earn a certificate</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                    auctor scelerisque nunc sit amet posuere.
+                </p>
+            </div>
+            <div className='card'>
+                <img
+                    src={require('./img/organization.png')}
+                    alt='Organization card'
+                />
+                <h3>Upskill your organization</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                    auctor scelerisque nunc sit amet posuere.
+                </p>
+            </div>
+        </div>
+    </Fragment>
+);
+
+export default Goals;
---
+++ client/src/components/layout/Landing/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import Goals from './Goals';
+
+import './Landing.css';
+
+const Landing = () => (
+    <div className='Landing'>
+        <Header />
+        <Goals />
+    </div>
+);
+
+export default Landing;
