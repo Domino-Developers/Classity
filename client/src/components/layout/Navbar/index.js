1bcb7570ee0f704e998c19856344f351a8fe0d79
Gurkirat Singh <gurkirat.singh@students.iiit.ac.in>
2020-06-22 19:34:40 +0530

    Revert "[Add] Auth Component (#126)"
    This reverts commit 9288d6410baa0ada65fcbeb61cf78d795955838c.

--- client/src/components/layout/Navbar/index.js
+++ client/src/components/layout/Navbar/index.js
@@ -1,44 +1,23 @@
-import React, { Fragment } from 'react';
-import { useLocation, Link } from 'react-router-dom';
-
-// components
+import React from 'react';
 import Button from '../Button';
-import Auth from '../../auth/Auth';
 
 import './Navbar.css';
 
-// custom hook for query param
-function useQuery(location) {
-    return new URLSearchParams(location.search);
-}
-
-const Navbar = () => {
-    let location = useLocation();
-    let query = useQuery(location);
-
-    return (
-        <Fragment>
-            <nav className='navbar bg-dark'>
-                <h1> Study Tube </h1>
-                <ul>
-                    <li>
-                        <a href='#!'>Explore</a>
-                    </li>
-                    <li>
-                        <a href='#!'>Log In</a>
-                    </li>
-                    <li className='bg-primary'>
-                        <Link to={`${location.pathname}?authMode=register`}>
-                            <Button text='Join for Free' full />
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-            {query.get('authMode') && (
-                <Auth mode={query.get('authMode')} path={location.pathname} />
-            )}
-        </Fragment>
-    );
-};
+const Navbar = () => (
+    <nav className='navbar bg-dark'>
+        <h1> Study Tube </h1>
+        <ul>
+            <li>
+                <a href='#!'>Explore</a>
+            </li>
+            <li>
+                <a href='#!'>Log In</a>
+            </li>
+            <li className='bg-primary'>
+                <Button text='Join for Free' full />
+            </li>
+        </ul>
+    </nav>
+);
 
 export default Navbar;
