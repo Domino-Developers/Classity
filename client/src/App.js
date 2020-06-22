9288d6410baa0ada65fcbeb61cf78d795955838c
Gurkirat Singh <gurkirat.singh@students.iiit.ac.in>
2020-06-22 18:39:30 +0530

    [Add] Auth Component (#126)

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+// components
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 
@@ -7,10 +10,12 @@ import './App.css';
 function App() {
     return (
         <Fragment>
-            <Navbar />
-            <div className='main'>
-                <Landing />
-            </div>
+            <Router>
+                <Navbar />
+                <div className='main'>
+                    <Landing />
+                </div>
+            </Router>
         </Fragment>
     );
 }
--- client/src/components/layout/Navbar/index.js
+++ client/src/components/layout/Navbar/index.js
@@ -1,23 +1,44 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { useLocation, Link } from 'react-router-dom';
+
+// components
 import Button from '../Button';
+import Auth from '../../auth/Auth';
 
 import './Navbar.css';
 
-const Navbar = () => (
-    <nav className='navbar bg-dark'>
-        <h1> Study Tube </h1>
-        <ul>
-            <li>
-                <a href='#!'>Explore</a>
-            </li>
-            <li>
-                <a href='#!'>Log In</a>
-            </li>
-            <li className='bg-primary'>
-                <Button text='Join for Free' full />
-            </li>
-        </ul>
-    </nav>
-);
+// custom hook for query param
+function useQuery(location) {
+    return new URLSearchParams(location.search);
+}
+
+const Navbar = () => {
+    let location = useLocation();
+    let query = useQuery(location);
+
+    return (
+        <Fragment>
+            <nav className='navbar bg-dark'>
+                <h1> Study Tube </h1>
+                <ul>
+                    <li>
+                        <a href='#!'>Explore</a>
+                    </li>
+                    <li>
+                        <a href='#!'>Log In</a>
+                    </li>
+                    <li className='bg-primary'>
+                        <Link to={`${location.pathname}?authMode=register`}>
+                            <Button text='Join for Free' full />
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+            {query.get('authMode') && (
+                <Auth mode={query.get('authMode')} path={location.pathname} />
+            )}
+        </Fragment>
+    );
+};
 
 export default Navbar;
