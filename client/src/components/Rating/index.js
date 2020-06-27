faaa960017abd71624320296e6a8a1bb00c59674
Sanchit Arora <56799401+tichnas@users.noreply.github.com>
2020-06-28 00:53:52 +0530

    [Fix] Modify Rating Component for half star (#159)

--- client/src/components/Rating/index.js
+++ client/src/components/Rating/index.js
@@ -5,27 +5,24 @@ import './Rating.css';
 
 const Rating = props => {
     const rating = Number(props.rating);
+    const fullWidth = (7.5 * rating) / 5 + 'rem';
+    const emptyWidth = (7.5 * (5 - rating)) / 5 + 'rem';
     return (
         <div className='star-rating'>
-            {rating >= 1 ? <FullStar /> : <EmptyStar />}
-            {rating >= 2 ? <FullStar /> : <EmptyStar />}
-            {rating >= 3 ? <FullStar /> : <EmptyStar />}
-            {rating >= 4 ? <FullStar /> : <EmptyStar />}
-            {rating >= 5 ? <FullStar /> : <EmptyStar />}
+            <div
+                className='star-rating-full'
+                style={{ width: fullWidth }}
+            ></div>
+            <div
+                className='star-rating-empty'
+                style={{ width: emptyWidth }}
+            ></div>
         </div>
     );
 };
 
-const FullStar = () => (
-    <img src={require('../../img/star-full.png')} alt='Full star' />
-);
-
-const EmptyStar = () => (
-    <img src={require('../../img/star-empty.png')} alt='Empty star' />
-);
-
 Rating.propTypes = {
-    rating: PropTypes.string.isRequired
+    rating: PropTypes.number.isRequired
 };
 
 export default Rating;
