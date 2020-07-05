879948f09e58dbd4fbd58b4eb5b911951fb89e64
gsc2001 <gurkirat.singh@students.iiit.ac.in>
2020-07-05 23:43:58 +0530

    [Add] Proxy error fix

--- server/server.js
+++ server/server.js
@@ -19,4 +19,4 @@ app.use('/api/test', require('./routes/api/test'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server started on port ${PORT}`));
