d951207c7122a920745ffea1ad05fbf291e1dcaf
Sanchit Arora <56799401+tichnas@users.noreply.github.com>
2020-07-08 00:04:55 +0530

    [Add] Option to add different type of resources (#228)
    Fixes #227

--- client/src/components/AddNew/index.js
+++ client/src/components/AddNew/index.js
@@ -3,14 +3,16 @@ import React from 'react';
 import './AddNew.css';
 
 const AddNew = props => {
-    const { onAdd, children } = props;
+    const children = React.Children.toArray(props.children);
 
     return (
         <div className='add-new'>
-            <a href='#!' onClick={onAdd}>
-                <i className='fas fa-plus'></i>
-                {children}
-            </a>
+            {children.map((child, i) => (
+                <a href='#!' onClick={child.props.onAdd} className='add-new-link' key={i}>
+                    <i className='fas fa-plus'></i>
+                    {child.props.children}
+                </a>
+            ))}
         </div>
     );
 };
