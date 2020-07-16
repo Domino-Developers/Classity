838dbbbebe54077d1795a0cd7cdcd0f28f51fdfa
tichnas <sanchit.arora.2002@gmail.com>
2020-07-17 01:47:40 +0530

    Change Loading component Fixes #324

--- client/src/components/Loading/index.js
+++ client/src/components/Loading/index.js
@@ -1,5 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-const Loading = () => <div className='loading'></div>;
+const Loading = props => {
+    const sizeInRem = props.size || '5';
+    const size = sizeInRem * parseFloat(getComputedStyle(document.documentElement).fontSize);
+    const strokeWidth = 2;
+    const center = size / 2;
+    const radius = center - strokeWidth / 2;
+    const circumference = 2 * Math.PI * radius;
+
+    const [offset, setOffset] = useState(50);
+
+    useEffect(() => {
+        const interval = setInterval(() => setOffset(offset - circumference / 20), 25);
+
+        return () => clearInterval(interval);
+    }, [offset, circumference]);
+
+    return (
+        <svg
+            className={
+                'loading ' +
+                (props.inline ? 'loading--inline ' : '') +
+                (props.className ? props.className : '')
+            }
+            width={size}
+            height={size}>
+            <circle
+                className='loading__circle loading__circle--inner'
+                cx={center}
+                cy={center}
+                r={radius}
+                strokeWidth={strokeWidth}
+                strokeDasharray={circumference}
+                strokeDashoffset={offset}
+            />
+        </svg>
+    );
+};
+
+Loading.propTypes = {
+    size: PropTypes.string,
+    className: PropTypes.string,
+    inline: PropTypes.bool
+};
 
 export default Loading;
