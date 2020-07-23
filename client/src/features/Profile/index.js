8d9dea3b2b166f7872d8c40f2c4f3251fe9e604c
tichnas <sanchit.arora.2002@gmail.com>
2020-07-23 19:50:18 +0530

    LeaderBoard + Profile for every user Fixes #380

--- client/src/features/Profile/index.js
+++ client/src/features/Profile/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import loadable from '@loadable/component';
 import { Link, useParams } from 'react-router-dom';
 import useSWR from 'swr';
+<<<<<<< HEAD
 import { createSelector } from '@reduxjs/toolkit';
 import { useSelector } from 'react-redux';
+=======
+>>>>>>> LeaderBoard + Profile for every user
 
 import Loading from '../../components/Loading';
 import { getxycoordinates } from './helper';
@@ -13,8 +16,11 @@ const LineChart = loadable(() => import('./LineChart'), { fallback: <Loading /> 
 
 // Assumption: score or contribution will NOT be >= 1000
 
+<<<<<<< HEAD
 const sel = createSelector([state => state.user], myUser => myUser);
 
+=======
+>>>>>>> LeaderBoard + Profile for every user
 const Bar = ({ start, end, exact, progress }) => (
     <div className='profile-bar'>
         <div className='profile-bar__filled' style={{ width: `${progress}%` }}>
@@ -29,6 +35,7 @@ const Bar = ({ start, end, exact, progress }) => (
 
 const Profile = () => {
     const { userId } = useParams();
+<<<<<<< HEAD
     const myUser = useSelector(sel);
 
     let { data: user } = useSWR(myUser._id !== userId ? `get-user-${userId}` : null, () =>
@@ -36,6 +43,10 @@ const Profile = () => {
     );
 
     if (userId === myUser._id) user = myUser;
+=======
+
+    const { data: user } = useSWR(`get-user-${userId}`, () => userStore.getUserById(userId));
+>>>>>>> LeaderBoard + Profile for every user
 
     if (!user) return <Loading />;
 
